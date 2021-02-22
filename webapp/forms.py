89e3b0f36404daecee2e28569b449b7efe296e62
Serhii S <thesergeys@gmail.com>
2021-02-22 20:25:25 +0200

    --1--

--- webapp/forms.py
+++ webapp/forms.py
@@ -4,19 +4,13 @@ from wtforms.validators import DataRequired, InputRequired, ValidationError
 import datetime as dt
 
 # https://wtforms.readthedocs.io/en/2.3.x/validators/?highlight=datarequired#wtforms.validators.DataRequired
-# def validate_duedate_for_task(form, field):
-#     try:
-#         duedate_task = dt.datetime.strptime(field.data, "%d.%m.%Y")
-#         # All OK
-#     except ValueError:
-#         raise ValidationError('format Date must be in XX.XX.XXXX format')
 
 def action_for_task():
 
     choices = []
     # Getting All Friends or Followers for a User
-    choices.append(('Friends',   'Friends'))
-    choices.append(('Followers', 'Followers'))
+    choices.append(('friends',   'Friends'))
+    choices.append(('followers', 'Followers'))
     # Analyzing a User’s Favorite Tweets
     choices.append(('favorite_tw', 'User’s Favorite Tweets'))
 
@@ -31,14 +25,15 @@ class AddTaskForm(FlaskForm):
     next = SubmitField('Next')
     cancel = SubmitField('Cancel')
 
+
+def my_id_name_check(form, field):
+    if not form.screen_name.data and not form.user_id.data:
+        raise ValidationError('You must fill in field "screen name" or "userid"')
+
 class UserForm(FlaskForm):
 
-    # description_task = StringField('Description for task', validators=[InputRequired()])
-    # duedate_for_task = DateField('Due date', format = '%d.%m.%Y', validators=[DataRequired()])
-    # action_for_task = SelectField('Action', choices = action_for_task(), validate_choice = True)
-
-    user_id     = StringField('user id', validators=[InputRequired()])
-    screen_name = StringField('screen name', validators=[InputRequired()])
+    user_id     = StringField('user id', validators=[my_id_name_check])
+    screen_name = StringField('screen name', validators=[my_id_name_check])
 
     next        = SubmitField('Next')
     cancel      = SubmitField('Cancel')
--- webapp/routes.py
+++ webapp/routes.py
@@ -1,5 +1,5 @@
 from webapp import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for, request
 from webapp.forms import AddTaskForm, UserForm
 import webapp.datastorage as datastorage
 
@@ -14,41 +14,74 @@ def addtask():
     form = AddTaskForm()
     # conn = datastorage.init()
 
+    # Cancel
+    if form.cancel.data == True:
+        return redirect('/index')
+
     if form.validate_on_submit():
 
         # flash('Add task: {} with duedate: {}'.format(form.description_task.data, form.duedate_for_task.data))
         # datastorage.add_task(conn, form.description_task.data, form.duedate_for_task.data)
 
-        # Cancel
-        if form.cancel.data == True:
-            return redirect('/index')
-
-        if form.action_for_task.data == "Friends":
-            return redirect('/userform')
-        elif form.action_for_task.data == "Followers":
-            return redirect('/userform')
+        if form.action_for_task.data == "friends":
+            # '/addtask?action=friends'
+            return redirect(url_for('addtask', action = form.action_for_task.data))
+            # return redirect('/addtask/friends')
+        elif form.action_for_task.data == "followers":
+            return redirect(url_for('addtask', action = form.action_for_task.data))
 
     return render_template('AddTask.html', title = 'Add new task', form=form)
 
 
-@app.route('/userform', methods=['GET', 'POST'])
-def userform():
+@app.route('/addtask/action', methods=['GET', 'POST'])
+def friends(action):
+    # userform('friends')
+    form = UserForm()
+    # conn = datastorage.init()
+
+    # Cancel
+    if form.cancel.data == True:
+        return redirect('/index')
+
+    if form.validate_on_submit():
+
+        if form.user_id.data:
+            flash('Add task for id: {}, action: {}'.format(form.user_id.data, 'friends'))
+            # datastorage.add_task(conn, form.description_task.data, form.duedate_for_task.data)
+            return redirect('/index')
+
+        elif form.screen_name.data:
+            flash('Add task for screen name: {}, action: {}'.format(form.screen_name.data, 'friends'))
+            # datastorage.add_task(conn, form.description_task.data, form.duedate_for_task.data)
+            return redirect('/index')
+
+    return render_template('UserForm.html', title='User ID or Screen name', form=form)
+
+# @app.route('/addtask/followers', methods=['GET', 'POST'])
+# def followers():
+#     userform('followers')
+
+
+def userform(action):
 
     form = UserForm()
     # conn = datastorage.init()
 
+    # Cancel
+    if form.cancel.data == True:
+        return redirect('/index')
+
     if form.validate_on_submit():
 
-        # Cancel
-        if form.cancel.data == True:
+        if form.user_id.data:
+            flash('Add task for id: {}, action: {}'.format(form.user_id.data, action))
+            # datastorage.add_task(conn, form.description_task.data, form.duedate_for_task.data)
             return redirect('/index')
 
-        if form.action_for_task.data == "Friends":
+        elif form.screen_name.data:
+            flash('Add task for screen name: {}, action: {}'.format(form.screen_name.data, action))
             # datastorage.add_task(conn, form.description_task.data, form.duedate_for_task.data)
-            return redirect('/userform', form.action_for_task.data)
-        elif form.action_for_task.data == "Followers":
-            # datastorage.add_task(conn, form.description_task.data, form.duedate_for_task.data)
-            return redirect('/userform', form.action_for_task.data)
+            return redirect('/index')
 
     return render_template('UserForm.html', title='User ID or Screen name', form=form)
 
